342749047100424e9a96e3d24aab980dc8ab398b
farronf <farronflaherty@gmail.com>
2025-06-17 23:44:04 +0100

    Add validation to CreateCompanyDTO

--- CompanyApi/DTOs/CreateCompanyDto.cs
+++ CompanyApi/DTOs/CreateCompanyDto.cs
@@ -6,12 +6,19 @@ namespace CompanyApi.DTOs
     {
         [Required]
         public string Name { get; set; }
+        
         [Required]
         public string Exchange { get; set; }
+        
         [Required]
         public string Ticker { get; set; }
+
         [Required]
+        [MinLength(12)]
+        [MaxLength(12)]
+        [RegularExpression(@"^[^\d]{2}.*$", ErrorMessage = "ISIN must start with two non-numeric characters.")]
         public string Isin { get; set; }
+        
         public string? Website { get; set; }
     }
 }
