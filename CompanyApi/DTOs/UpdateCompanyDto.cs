a4166ea425af31ae763d03fcee001ff0bcffd34d
farronf <farronflaherty@gmail.com>
2025-06-17 21:22:48 +0100

    Add UpdateCompanyDto

---
+++ CompanyApi/DTOs/UpdateCompanyDto.cs
@@ -0,0 +1,8 @@
+public class UpdateCompanyDto
+{
+    public string? Name { get; set; }
+    public string? Exchange { get; set; }
+    public string? Ticker { get; set; }
+    public string? Isin { get; set; }
+    public string? Website { get; set; }
+}
